7e45af4f09e82c170823d6a24f3016a4cd9e6f0d
pdendy <PDENDY@Skillstorm.com>
2022-08-28 14:18:50 -0400

    "Start of Home page"

--- stormer-mobile-angular/src/app/app.component.ts
+++ stormer-mobile-angular/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'stormer-mobile-angular';
+  JoeImagePath: string;
+
+  constructor () {
+  this.JoeImagePath = 'src\assets\img\joeAndBro.png';
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
